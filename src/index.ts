8f134ff19fc70a5d0f5bed607fd502a8dae3b84c
Paula Ribeiro <contatopaular@gmail.com>
2024-11-01 08:53:21 -0300

    [chore] Removed no longer used files and functions

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
 import express from "express";
-import cors from "cors"
 import { router } from "./routes";
 import { connectionDB } from "./db/connection";
-import { populateDB } from "./db/startDB";
 const app = express();
-app.use(cors())
 const port = 3000;
 
 app.use(express.json());
@@ -15,8 +12,7 @@ app.use(router);
 
 
 connectionDB().catch(console.dir);
-populateDB()
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}/`);
 });
